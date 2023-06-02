4654f332bc706ce7c997f6d3620ca4c8917861c1
EdinaelsonGaleno <sedinaelson@gmail.com>
2023-06-02 13:34:42 -0300

    tratamentos

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
 import controller.ContaCorrente;
 import controller.ContaPoupanca;
+import exceçoes.SaldoInsuficiente;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ContaCorrente c1 = new ContaCorrente("2020",10,50);
-        ContaPoupanca c2 = new ContaPoupanca("2023", 1000, 40);
-        // c1.sacar(30);
-        // c1.depositar(500);
-        // c1.sacar(200);
-        // c1.sacar(700);
-        // c2.depositar(300);
-        // c2.sacar(100);
-        // c2.calcularRendimento();
-        // c2.sacar(1500);
+        try {
+
+            ContaCorrente c1 = new ContaCorrente("2020",10,50);
+            ContaPoupanca c2 = new ContaPoupanca("2023", 1000, 40);
+            c1.sacar(30);
+            c1.depositar(500);
+            c1.sacar(200);
+            c1.sacar(700);
+            c2.depositar(300);
+            c2.sacar(100);
+            c2.calcularRendimento();
+            c2.sacar(1500);
+
+        }catch (SaldoInsuficiente e){
+            System.out.println("Mensagem Error: " + e.getMessage());
+        }
     }
 }
--- src/exceçoes/SaldoInsuficiente.java
+++ src/exceçoes/SaldoInsuficiente.java
@@ -3,9 +3,12 @@ package exceçoes;
 public class SaldoInsuficiente extends Exception {
     
     public SaldoInsuficiente(){
-        
         System.out.println("Saldo insuficiente para saque! \n");
     }
-    
-    
+
+    @Override
+    public String getMessage() {
+        String message = "Saldo insuficiente para saque!";
+        return message;
+    }
 }
